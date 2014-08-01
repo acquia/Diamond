4a47fce18c87ebebd05052c26642594c911ab51a
jfarrell <jfarrell@apache.org>
2014-08-01 15:04:39 -0400

    Refactoring to prepare for cluster config

---
+++ packages/build_scripts/cassandra.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#
+# Script to download the latest Apache Cassandra debian package. Latest stable version
+# in the 20x repo is 2.0.9
+#
+set -x
+
+DEB_VERSION="20x"
+
+# Download and import the Cassandra GPG keys used to sign the release and deb mirror
+curl -s  http://www.apache.org/dist/cassandra/KEYS | gpg --import -
+gpg --export --armor | sudo apt-key add -
+
+# Add the Cassandra package mirror
+echo "deb http://www.apache.org/dist/cassandra/debian ${DEB_VERSION} main" > /etc/apt/sources.list.d/cassandra.list
+
+# Update and download the cassandra deb package
+apt-get update -qq
+apt-get -qq download cassandra
+
+# Clean up
+rm /etc/apt/sources.list.d/cassandra.list
+/usr/bin/apt-get update -qq
+
+# If in a VM copy then deb file over
+if [ -d "/vagrant/" ]; then
+  mv -f *.deb /vagrant/
+fi
---
+++ puppet/modules/base/lib/puppet/parser/functions/cloudformation_output.rb
@@ -0,0 +1,19 @@
+require 'aws-sdk'
+
+module Puppet::Parser::Functions
+  # Lookup an output result for a given CloudFormation stack.
+  #
+  # @param template [String] - CloudFormation Template name
+  # @param output [String] - CloudFormation Output name
+  # @return [AWS::CloudFormation::StackOutput] or nil
+  newfunction(:cloudformation_output, :type => :rvalue) do |args|
+    tempalate_name = args[0]
+    output_name = args[1]
+
+    cf = Nemesis::Aws::Sdk::CloudFormation.new
+    stack = cf.stacks[tempalate_name]
+
+    output = stack.outputs.detect { |x| x.key == output_name }
+    output
+  end
+end
