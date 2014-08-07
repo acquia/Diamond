c4ec8057b1dbca061bfffda69f5596fffaadf809
Dan Norris <daniel.norris@acquia.com>
2014-08-07 11:17:53 -0400

    Fix repo template references and ruby functions

--- puppet/modules/base/manifests/repos.pp
+++ puppet/modules/base/manifests/repos.pp
@@ -1,10 +1,10 @@
 class base::repos {
   include apt
 
-  $stack_name = cloudformation_stackname(@ec2_instance_id)
+  $stack_name = cloudformation_stackname($ec2_instance_id)
 
   apt::source { 'nemesis':
-    location    => cloudformation_output(@stack_name, 'AptMirrorURL'),
+    location    => cloudformation_output($stack_name, 'AptMirrorURL'),
     release     => $::lsbdistcodename,
     repos       => 'main',
     key         => '23406CA7',
