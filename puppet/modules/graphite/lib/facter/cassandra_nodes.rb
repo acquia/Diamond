f4521d300f4bf71e5255d735536d8c7951749184
jfarrell <jfarrell@apache.org>
2014-08-18 12:35:03 -0400

    Fix graphite cassandra nodes list to use Cloudformation exported config file

--- puppet/modules/graphite/lib/facter/cassandra_nodes.rb
+++ puppet/modules/graphite/lib/facter/cassandra_nodes.rb
@@ -1,16 +1,14 @@
-require 'facter'
-require 'aws-sdk'
+require "json"
 
 Facter.add('cassandra_nodes') do
   setcode do
-    cf = AWS::CloudFormation.new
-    ec2 = AWS::EC2.new
-    stack = cf.stack_resource(Facter.value(:ec2_instance_id)).stack
-    cassandra_stack = stack.outputs.select{|o| o.key == 'CassandraStack'}.first.value
-    cassandra_nodes = cf.stacks[cassandra_stack].outputs.select{|s| s.key == 'CassandraClusterList'}
-    unless cassandra_nodes.empty?
-      nodes = cassandra_nodes.first.value
-      nodes.split(',').map{|s| "'#{ec2.instances[s].ip_address}:9160'"}.join(',')
+    config_file = "/etc/graphite.json"
+    nodes = ""
+    if File.exists?(config_file)
+      # Load the json overrides config
+      config = JSON.parse(File.read(config_file))
+      nodes = config['cassandra_cluster']
     end
+    nodes
   end
 end
