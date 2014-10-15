55e642a691deb758c11cfe23ee4d670729c95f70
jfarrell <jfarrell@apache.org>
2014-10-15 16:36:14 -0400

    Removing need for cassandra overrides file

---
+++ puppet/modules/cassandra/lib/facter/cassandra_seed.rb
@@ -0,0 +1,10 @@
+require 'facter'
+require 'aws-sdk'
+
+Facter.add(:cassandra_seed) do
+  setcode do
+    ec2 = AWS::EC2.new
+    tags = ec2.instances[Facter.value('ec2_instance_id')].tags.to_h
+    tags['seed'] == true
+  end
+end
