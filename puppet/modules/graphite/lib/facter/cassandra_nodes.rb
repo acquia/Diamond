1881a02348148292cd2ac84c581854b902dd6fcb
jfarrell <jfarrell@apache.org>
2014-12-05 16:24:16 -0500

    Update check for cassandra nodes

--- puppet/modules/graphite/lib/facter/cassandra_nodes.rb
+++ puppet/modules/graphite/lib/facter/cassandra_nodes.rb
@@ -15,7 +15,7 @@ if ec2.instances[Facter.value('ec2_instance_id')].tags.to_h['server_type'] == 'g
       setcode do
         cassandra_cluster = []
         ["Seed", "Node"].each do |type|
-          if cf.stacks[cassandra_stack_name].resources.map(&:physical_resource_id).include?("Cassandra#{type}sAutoScalingGroup")
+          if cf.stacks[cassandra_stack_name].resources.map(&:logical_resource_id).include?("Cassandra#{type}sAutoScalingGroup")
             seed_autoscaling_group = cf.stacks[cassandra_stack_name].resources["Cassandra#{type}sAutoScalingGroup"].physical_resource_id
             autoscaling_group = AWS::AutoScaling::Group.new(seed_autoscaling_group)
             autoscaling_group.auto_scaling_instances.each { |i| cassandra_cluster << ec2.instances[i.id].ip_address }
