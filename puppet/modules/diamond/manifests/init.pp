fbd5a68157aa7fe022ba8f854565b7da97036ce2
Kevin Hankens <email@kevinhankens.com>
2015-01-14 13:26:43 -0500

    Send JMX metrics to cloudwatch/archive on behalf of Cassandra.

--- puppet/modules/diamond/manifests/init.pp
+++ puppet/modules/diamond/manifests/init.pp
@@ -1,4 +1,6 @@
-class diamond {
+class diamond (
+  $cassandra = false
+){
   require base::repos
   require diamond::packages
 
@@ -41,12 +43,51 @@ class diamond {
     require => Package['diamond'],
   }
 
-  file {'/etc/diamond/handlers/cloudwatchHandler.conf':
+  concat {'/etc/diamond/handlers/cloudwatchHandler.conf':
+    ensure => present,
+    notify  => Service['diamond'],
     owner   => 'diamond',
     group   => 'diamond',
     mode    => '0644',
     require => [ Package['diamond'], File['/etc/diamond/handlers'] ],
-    content => template('diamond/cloudwatchHandler.conf.erb'),
-    notify  => Service['diamond'],
   }
+
+  concat::fragment {'cloudwatchHandlerHeader':
+    target  => '/etc/diamond/handlers/cloudwatchHandler.conf',
+    content => template('diamond/cloudwatchHandler.conf.erb'),
+    order   => '01',
+  }
+
+  concat::fragment {'cloudwatchHandlerDefault':
+    target  => '/etc/diamond/handlers/cloudwatchHandler.conf',
+    source  => 'puppet:///modules/diamond/cloudwatch_handlers/default.conf',
+    order   => '02',
+  }
+
+  file {'/etc/diamond/collectors':
+    ensure  => directory,
+    owner   => 'diamond',
+    group   => 'diamond',
+    mode    => '0644',
+    require => Package['diamond'],
+  }
+
+  if ($cassandra) {
+    concat::fragment {'cloudwatchHandlerCassandra':
+      target  => '/etc/diamond/handlers/cloudwatchHandler.conf',
+      source  => 'puppet:///modules/diamond/cloudwatch_handlers/cassandra.conf',
+      order   => '03',
+    }
+
+    file {'/etc/diamond/collectors/CassandraCollector.conf':
+      owner   => 'diamond',
+      group   => 'diamond',
+      mode    => '0644',
+      require => File['/etc/diamond/collectors'],
+      source  => 'puppet:///modules/diamond/CassandraCollector.conf',
+      notify  => Service['diamond'],
+    }
+  }
+
+
 }
