e09513754143b9d60fc883f536db1439e9b0c2a3
kasisnu <kasisnu@gmail.com>
2015-01-16 14:39:56 +0530

    Zookeeper - Puppet module to work with ASG(d9aba70)

---
+++ puppet/modules/zookeeper/lib/facter/zookeeper_info.rb
@@ -0,0 +1,40 @@
+require 'facter'
+require 'aws-sdk'
+
+ec2 = AWS::EC2.new
+
+if ec2.instances[Facter.value('ec2_instance_id')].tags.to_h['server_type'] == 'zookeeper'
+  cf = AWS::CloudFormation.new
+  stack = cf.stack_resource(Facter.value('ec2_instance_id'))
+  stack_name = stack.stack_name
+
+  Facter.add('zk_config_location') do
+    setcode do
+      cf.stacks[stack_name].resources['NemesisZookeeperS3Bucket'].physical_resource_id
+    end
+  end
+
+  Facter.add('zk_exhibitor_aws_access_key_id') do
+    setcode do
+      AWS::config.credentials[:access_key_id]
+    end
+  end
+
+  Facter.add('zk_exhibitor_aws_secret_access_key') do
+    setcode do
+      AWS::config.credentials[:secret_access_key]
+    end
+  end
+  
+  Facter.add('zk_s3_prefix') do
+    setcode do
+      "exhibitor"
+    end
+  end
+
+  Facter.add('zk_exhibitor_ui_password') do
+    setcode do
+      stack.stack.parameters['ExhibitorUiPassword']
+    end
+  end
+end
---
+++ puppet/modules/zookeeper/manifests/init.pp
@@ -0,0 +1,57 @@
+class zookeeper{
+  require base::repos
+  require java
+  
+  package { 'zookeeper' :
+    ensure => latest,
+  }
+
+  package { 'zookeeper-exhibitor' :
+    ensure => latest,
+    require => [ Package['zookeeper'], ],
+  }
+
+  file { ['/opt/zookeeper', '/opt/zookeeper/transactions', '/opt/zookeeper/snapshots' ]:
+    ensure => directory,
+    require => [ Package['zookeeper'], ],
+  }
+
+  file { '/opt/exhibitor/web.xml':
+    ensure  => present,
+    source  => 'puppet:///modules/zookeeper/web.xml',
+    mode    => 'a+x',
+    require => [ Package['zookeeper-exhibitor'], ],
+  }
+
+  file { '/opt/exhibitor/defaults.conf':
+    ensure => present,
+    content => template('zookeeper/defaults.conf.erb'), 
+    require => [ Package['zookeeper-exhibitor'], ],
+  }
+  
+  file { '/opt/exhibitor/credentials.properties':
+    ensure => present,
+    content => template('zookeeper/credential.properties.erb'),
+    require => [ Package['zookeeper-exhibitor'], ],
+  }
+
+  file { '/etc/init.d/exhibitor':
+    ensure  => present,
+    content => template('zookeeper/init.d.erb'),
+    mode    => '0755',
+    require => [ Package['zookeeper-exhibitor'], ],
+  }
+
+  file { '/opt/exhibitor/realm':
+    ensure  => present,
+    content => template('zookeeper/realm.erb'),
+    mode    => '0755',
+    require => [ Package['zookeeper-exhibitor'], ],
+  }
+  
+  service { 'exhibitor':
+    ensure => 'running',
+    require => [ File['/etc/init.d/exhibitor'], File['/opt/exhibitor/realm'], File['/opt/exhibitor/web.xml'], File['/opt/exhibitor/defaults.conf'], File['/opt/exhibitor/credentials.properties'], ],
+  }
+
+}
