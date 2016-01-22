2481d8733a4e2f91d3b7828c32abd23d4c96ad32
James Hsiao <james.hsiao@acquia.com>
2016-01-22 08:34:21 -0500

    Apply puppet util

---
+++ puppet/util/apply_puppet.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+# Run puppet apply without updating nemesis-puppet package or sending output to /var/log/puppet.log
+#
+
+# Export all necessary environment variables
+export RUBYLIB=/etc/puppet/lib:$RUBYLIB
+export AMAZON_REGION=$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone | sed 's/[a-z]$//')
+
+# Run puppet
+cd /etc/puppet && puppet apply manifests/nodes.pp --modulepath=/etc/puppet/modules:/etc/puppet/modules/third_party
