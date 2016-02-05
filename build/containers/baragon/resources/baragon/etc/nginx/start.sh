64640dfbecb74af2348ee19de55a2bdf0da946f5
kasisnu <kasisnu@gmail.com>
2016-02-05 13:47:26 +0530

    Run nginx on baragon agents as user "nginx", and remove default http config

--- build/containers/baragon/resources/baragon/etc/nginx/start.sh
+++ build/containers/baragon/resources/baragon/etc/nginx/start.sh
@@ -2,4 +2,6 @@
 # This overwrites default nginx vhost file.
 # Nginx gets confused when one is present and not used.
 echo "" > /etc/nginx/conf.d/vhost.conf
+# Remove default http settings
+rm -f /etc/nginx/conf.d/default.conf
 nginx
